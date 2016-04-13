39bb1704f7c9b0257d305ab66c074c9c1c3ffc72
Rahul Sharma <rsconceptx@gmail.com>
2016-04-13 23:48:35 +0530

    adding theme class and swapped todo div

--- client/events/navbar.js
+++ client/events/navbar.js
@@ -1,6 +1,12 @@
+import { Themer } from '../../lib/themer.js';
 
 Template.navbar.events({
 	'click #theme-btn': function() {
 		Session.set('show_progress', true);
+		setTimeout(() => {
+			let navbar = document.getElementById('navbar-theme');
+			navbar.className=Themer.changeTheme();
+			Session.set('show_progress', false);
+		},2000);
 	}
 })
